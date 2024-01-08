e0d3f459f44e2ce11756af88fb92a9a5c91bbb9a
Parami <paramiashinsana@gmail.com>
2024-01-08 13:42:19 +0530

    update StudentDTO

--- src/main/java/com/example/exampledemo/dto/StudentDTO.java
+++ src/main/java/com/example/exampledemo/dto/StudentDTO.java
@@ -1,4 +1,15 @@
 package com.example.exampledemo.dto;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Data
 public class StudentDTO {
+    private String id;
+    private String name;
+    private String address;
 }
