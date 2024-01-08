a1a23719c0b580aa3d7598d5e006f781a958d760
Parami <paramiashinsana@gmail.com>
2024-01-08 13:57:07 +0530

    get the connection

--- src/main/java/com/example/exampledemo/Item.java
+++ src/main/java/com/example/exampledemo/Item.java
@@ -6,9 +6,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
 
 @WebServlet(name = "ExampleDemo", urlPatterns = "/test")
 public class Item extends HttpServlet {
+
+    Connection connection;
+
+    @Override
+    public void init() throws ServletException {
+
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Parami Ashinsana");
